aa87db3aa31149a37a4bd0de41d243340c1a831a
charukirti <charukirticc112002@gmail.com>
2024-07-14 12:59:18 +0530

    Completed feature request challenges

---
+++ Day-02/06-feature.js
@@ -0,0 +1,31 @@
+// Feature 1
+
+let x = 12;
+let y = 6;
+
+console.log('Addition is', x + y); // Output:- Addition is 18
+console.log('Subtraction is', x - y); // Output:- Subtraction is 6
+console.log('Multiplication is', x * y); // Output:- Multiplication is 72
+console.log('Division is', x / y); // Output :- Division is 2
+console.log('Remainder is', x % y); // Output:- Remainder is 0
+
+// Feature 2
+
+let a = 7;
+let b = 14;
+
+console.log('a > b:', a > b);    // Output:- a > b: false
+console.log('a < b:', a < b);    // Output:- a < b: true
+console.log('a == b:', a == b);  // Output:- a == b: false
+console.log('a != b:', a != b);  // Output:- a != b: true
+console.log('a >= b:', a >= b);  // Output:- a >= b: false
+console.log('a <= b:', a <= b);  // Output:- a <= b: true
+
+console.log('a < b && a != b:', a < b && a != b);  // Output:- true 
+console.log('a > b || a == b:', a > b || a == b);  // Output:- false 
+console.log('!(a == b):', !(a == b));              // Output:- true 
+
+// Feature 3
+
+const number = 13;
+console.log(number >= 0 ? 'Number is positive' : 'Number is negative'); // Output:- Number is positive

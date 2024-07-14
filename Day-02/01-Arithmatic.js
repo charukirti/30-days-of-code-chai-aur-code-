c65a8c92ece7b64c05b4209c4deed3c05c8ce103
charukirti <charukirticc112002@gmail.com>
2024-07-14 11:35:39 +0530

    Completed activity 1

---
+++ Day-02/01-Arithmatic.js
@@ -0,0 +1,35 @@
+// Task 1 
+
+const a = 4;
+const b = 3;
+
+console.log(a + b); // Output:- 7
+
+// Task 2
+
+const numOne = 14;
+const numTwo = 7;
+
+console.log(numOne - numTwo); // Output:- 7
+
+// Task 3
+
+const numThree = 5;
+const numFour = 2;
+
+console.log(numThree * numFour); // Output:- 10
+
+// Task 4
+
+const numFive = 14;
+const numSix = 2;
+
+console.log(numFive / numSix) // Output:- 7
+
+// Task 5
+
+const numSeven = 15;
+const numEight = 2;
+
+console.log(numSeven % numEight); // Output:- 1
+

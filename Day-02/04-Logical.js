f3f2198e05e4545d755fd08916f02b9f8cbf9cf0
charukirti <charukirticc112002@gmail.com>
2024-07-14 12:27:23 +0530

    Completed activity 4

---
+++ Day-02/04-Logical.js
@@ -0,0 +1,20 @@
+// Task 11
+
+const a = 11;
+const b = 12;
+const c = 13;
+
+console.log((a < b) && (b < c)); // Output:- true
+
+// Task 12
+
+const x = 4;
+const y = 5;
+const z = 6;
+
+console.log((x < y) || (y > z)); // Output:- true
+
+// Task 13
+
+const isLoggedIn = true;
+console.log(!isLoggedIn); // Output:- false

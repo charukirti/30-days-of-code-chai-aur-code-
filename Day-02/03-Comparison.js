002358ec0594a4621cfb51afb2b176c320150331
charukirti <charukirticc112002@gmail.com>
2024-07-14 12:14:19 +0530

    Completed activity 3

---
+++ Day-02/03-Comparison.js
@@ -0,0 +1,24 @@
+// Task 8
+
+const num1 = 10;
+const num2 = 5;
+
+console.log(num2 < num1); // Output:- true
+console.log(num1 > num2); // Output:- true
+
+// Task 9
+
+const num3 = 11;
+const num4 = 10;
+
+console.log(num3 <= num4); // Output:- false
+console.log(num4 >= num3); // Output:- false
+
+// Task 10
+
+const num5 = 15;
+const num6 = '15';
+
+console.log(num5 == num6); // Output:- true
+console.log(num5 === num6); // Output:- false
+
